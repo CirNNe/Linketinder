271d9da2a203bb19ed44e0bd9ea9974c103fa110
Higor Cirne <cirnne1@outlook.com>
2023-03-08 07:17:11 -0300

    feat: Frontend(Empresa(Gráfico de Dados sobre os Candidatos))

--- src/Frontend/src/main.ts
+++ src/Frontend/src/main.ts
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    /* CANDIDATO */
+    /* ELEMENTOS GRÁFICO */
+    const canvas = document.getElementById("myChart") as HTMLCanvasElement
+
+    /* ELEMENTOS CANDIDATO */
     const telaOpcoesCandidato = document.querySelector("#conteudo-candidato") as HTMLDivElement
     const telaCadastroCandidato = document.querySelector("#conteudo-cadastro-candidato") as HTMLDivElement
     const telaVagas = document.querySelector("#conteudo-vagas") as HTMLDivElement
+    const telaEmpresas = document.querySelector("#conteudo-lista-empresas") as HTMLDivElement
 
     const botaoOpcaoCadastrarCandidato = document.querySelector("#botao-escolha-cadatrar-candidato") as HTMLButtonElement
     const botaoOpcaoVagasDisponiveis = document.querySelector("#botao-escolha-vagas") as HTMLButtonElement
@@ -17,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const inputDescricaoPessoalNovoCandidato = document.querySelector("#descricao-pessoal-novo-candidato") as HTMLInputElement
     const inputCompetencias = document.getElementsByName("competencias-novo-candidato") as unknown as Array<any>
 
-    /* EMPRESA */
+    /* ELEMENTOS EMPRESA */
     const telaOpcoesEmpresa = document.querySelector("#conteudo-empresa") as HTMLDivElement
     const telaCadastroEmpresa = document.querySelector("#conteudo-cadastro-empresa") as HTMLDivElement
     const telaCadastroVaga = document.querySelector("#conteudo-cadastro-vaga")! as HTMLDivElement
@@ -27,29 +31,88 @@ document.addEventListener('DOMContentLoaded', function () {
     const botaoOpcaoCadastrarVaga = document.querySelector("#botao-escolha-cadastrar-vaga") as HTMLButtonElement
     const botaoOpcaoListaCandidatos = document.querySelector("#botao-escolha-lista-candidatos") as HTMLButtonElement
     const botaoCadastrarNovaVaga = document.querySelector("#botao-cadastrar-nova-vaga")! as HTMLButtonElement
+    const botaoCadastrarNovaEmpresa = document.querySelector("#botao-cadastrar-nova-empresa")! as HTMLButtonElement
 
     const inputNomeNovaVaga = document.querySelector("#nome-nova-vaga") as HTMLInputElement
     const inputEmpresaNovaVaga = document.querySelector("#empresa-nova-vaga") as HTMLInputElement
     const inputDescricaoNovaVaga = document.querySelector("#descricao-nova-vaga") as HTMLInputElement
     const inputCompetenciasNovaVaga = document.getElementsByName("competencias-nova-vaga") as unknown as Array<any>
 
+    const inputNomeNovaEmpresa = document.querySelector("#nome-nova-empresa") as HTMLInputElement
+    const inputEmailNovaEmpresa = document.querySelector("#email-nova-empresa") as HTMLInputElement
+    const inputCnpjNovaEmpresa = document.querySelector("#cnpj-nova-empresa") as HTMLInputElement
+    const inputCepNovaEmpresa = document.querySelector("#cep-nova-empresa") as HTMLInputElement
+    const inputEstadoNovaEmpresa = document.querySelector("#estado-nova-empresa") as HTMLInputElement
+    const inputPaisNovaEmpresa = document.querySelector("#pais-nova-empresa") as HTMLInputElement
+    const inputDescricaoNovaEmpresa = document.querySelector("#descricao-nova-empresa") as HTMLInputElement
 
 
 
-    /* EMPRESA */
-    if (botaoOpcaoCadastrarEmpresa) {
+
+    /* FUNÇÕES EMPRESA */
+    if(botaoOpcaoCadastrarEmpresa) {
         botaoOpcaoCadastrarEmpresa.addEventListener('click', function () {
             telaOpcoesEmpresa.style.display = "none"
             telaCadastroEmpresa.style.display = "block"
         })
     }
 
-    if (botaoOpcaoCadastrarVaga) {
+    if(botaoOpcaoCadastrarVaga) {
         botaoOpcaoCadastrarVaga.addEventListener('click', function () {
             telaOpcoesEmpresa.style.display = "none"
             telaCadastroVaga.style.display = "block"
         })
     }
+
+    if(botaoCadastrarNovaEmpresa) {
+        botaoCadastrarNovaEmpresa.addEventListener('click', function () {
+
+            let empresa = {
+                id: gerarId(),
+                nome: inputNomeNovaEmpresa.value,
+                email: inputEmailNovaEmpresa.value,
+                cnpj: inputCnpjNovaEmpresa.value,
+                cep: inputCepNovaEmpresa.value,
+                estado: inputEstadoNovaEmpresa.value,
+                pais: inputPaisNovaEmpresa.value,
+                descricao: inputDescricaoNovaEmpresa.value
+            }
+
+            salvarEmpresaLocalStorage(empresa)
+            telaCadastroEmpresa.style.display = 'none'
+            telaOpcoesEmpresa.style.display = 'block'
+
+        })
+    }
+
+    function salvarEmpresaLocalStorage(empresa: object) {
+        let listaEmpresas = localStorage.getItem('empresas') as string
+        if(listaEmpresas) {
+            let listaEmpresasJson = []
+            listaEmpresasJson = JSON.parse(listaEmpresas)
+            listaEmpresasJson.push(empresa)
+            localStorage.setItem('empresas', JSON.stringify(listaEmpresasJson))
+        } else {
+            localStorage.setItem('empresas', '[]')
+            let listaEmpresasJson = []
+            listaEmpresasJson = JSON.parse(listaEmpresas)
+            listaEmpresasJson.push(listaEmpresas)
+            localStorage.setItem('empresas', JSON.stringify(listaEmpresasJson))
+        }
+    }
+
+    function mostrarListaEmpresas() {
+        const listaEmpresasLocalStorage = localStorage.getItem('empresas') as string
+        const listaEmpresas = JSON.parse(listaEmpresasLocalStorage)
+        if (listaEmpresasLocalStorage) {
+            for(let posicao = 0; posicao < listaEmpresas.length; posicao++) {
+                let div = criarTagDivEmpresa(listaEmpresas[posicao])
+                telaEmpresas.appendChild(div)
+                telaEmpresas.style.display = "block"
+            }
+        }
+    }
+
 
     if(botaoCadastrarNovaVaga) {
         botaoCadastrarNovaVaga.addEventListener('click', function () {
@@ -101,6 +164,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const listaVagas = JSON.parse(listaVagasLocalStorage)
         if (listaVagasLocalStorage) {
             for(let posicao = 0; posicao < listaVagas.length; posicao++) {
+                listaVagas[posicao].empresa = "Anônimo"
                 let div = criarTagDivVaga(listaVagas[posicao])
                 telaVagas.appendChild(div)
             }
@@ -115,7 +179,7 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
 
-    /* CANDIDATO */
+    /* FUNÇÕES CANDIDATO */
 
     if (botaoOpcaoCadastrarCandidato) {
         botaoOpcaoCadastrarCandidato.addEventListener('click', function () {
@@ -186,6 +250,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const listaCandidatos = JSON.parse(listaCandidatosLocalStorage)
         if(listaCandidatosLocalStorage) {
             for (let posicao = 0; posicao < listaCandidatos.length; posicao++) {
+                listaCandidatos[posicao].nome = "Anônimo"
                 let div = criaTagDivCandidato(listaCandidatos[posicao])
                 telaListaCandidatos.appendChild(div)
             }
@@ -219,17 +284,17 @@ document.addEventListener('DOMContentLoaded', function () {
         liNomeCandidato.classList.add('dados-usuario')
         liNomeCandidato.innerHTML = candidato.nome
 
-        let pEmailCandidato = document.createElement('p')
-        pEmailCandidato.innerHTML = 'E-mail:'
-        pEmailCandidato.classList.add('estilo-p')
+        let pEmailEmpresa = document.createElement('p')
+        pEmailEmpresa.innerHTML = 'E-mail:'
+        pEmailEmpresa.classList.add('estilo-p')
 
-        let liEmailCandidato = document.createElement('li')
-        liEmailCandidato.classList.add('dados-usuario')
-        liEmailCandidato.innerHTML = candidato.email
+        let liEmailEmpresa = document.createElement('li')
+        liEmailEmpresa.classList.add('dados-usuario')
+        liEmailEmpresa.innerHTML = candidato.email
 
-        let pIdadeCandidato = document.createElement('p')
-        pIdadeCandidato.innerHTML = 'Idade:'
-        pIdadeCandidato.classList.add('estilo-p')
+        let pIdadeEmpresa = document.createElement('p')
+        pIdadeEmpresa.innerHTML = 'Idade:'
+        pIdadeEmpresa.classList.add('estilo-p')
 
         let liIdadeCandidato = document.createElement('li')
         liIdadeCandidato.classList.add('dados-usuario')
@@ -271,9 +336,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
         ul.appendChild(pNomeCandidato)
         ul.appendChild(liNomeCandidato)
-        ul.appendChild(pEmailCandidato)
-        ul.appendChild(liEmailCandidato)
-        ul.appendChild(pIdadeCandidato)
+        ul.appendChild(pEmailEmpresa)
+        ul.appendChild(liEmailEmpresa)
+        ul.appendChild(pIdadeEmpresa)
         ul.appendChild(liIdadeCandidato)
         ul.appendChild(pCpfCandidato)
         ul.appendChild(liCpfCandidato)
@@ -338,7 +403,156 @@ document.addEventListener('DOMContentLoaded', function () {
         div.appendChild(br)
 
         return div
-
     }
+
+    function criarTagDivEmpresa(empresa: any) {
+        let div = document.createElement('div')
+        div.id = empresa.id
+
+        let ul = document.createElement("ul")
+        ul.classList.add("estilo-ul")
+
+        let pNomeEmpresa = document.createElement('p')
+        pNomeEmpresa.innerHTML = 'Nome:'
+        pNomeEmpresa.classList.add('estilo-p')
+        let liNomeEmpresa = document.createElement('li')
+        liNomeEmpresa.classList.add('dados-usuario')
+        liNomeEmpresa.innerHTML = empresa.nome
+
+        let pEmailEmpresa = document.createElement('p')
+        pEmailEmpresa.innerHTML = 'E-mail:'
+        pEmailEmpresa.classList.add('estilo-p')
+        let liEmailEmpresa = document.createElement('li')
+        liEmailEmpresa.classList.add('dados-usuario')
+        liEmailEmpresa.innerHTML = empresa.email
+
+        let pCnpjEmpresa = document.createElement('p')
+        pCnpjEmpresa.innerHTML = 'CNPJ:'
+        pCnpjEmpresa.classList.add('estilo-p')
+        let liCnpjEmpresa = document.createElement('li')
+        liCnpjEmpresa.classList.add('dados-usuario')
+        liCnpjEmpresa.innerHTML = empresa.cnpj
+
+        let pCepEmpresa = document.createElement('p')
+        pCepEmpresa.innerHTML = 'CEP:'
+        pCepEmpresa.classList.add('estilo-p')
+        let liCepEmpresa = document.createElement('li')
+        liCepEmpresa.classList.add('dados-usuario')
+        liCepEmpresa.innerHTML = empresa.cep
+
+        let pEstadoEmpresa = document.createElement('p')
+        pEstadoEmpresa.innerHTML = 'Estado:'
+        pEstadoEmpresa.classList.add('estilo-p')
+        let liEstadoEmpresa = document.createElement('li')
+        liEstadoEmpresa.classList.add('dados-usuario')
+        liEstadoEmpresa.innerHTML = empresa.estado
+
+        let pPaisEmpresa = document.createElement('p')
+        pPaisEmpresa.innerHTML = 'País:'
+        pPaisEmpresa.classList.add('estilo-p')
+        let liPaisEmpresa = document.createElement('li')
+        liPaisEmpresa.classList.add('dados-usuario')
+        liPaisEmpresa.innerHTML = empresa.pais
+
+        let pDescricaoEmpresa = document.createElement('p')
+        pDescricaoEmpresa.innerHTML = 'Descrição:'
+        pDescricaoEmpresa.classList.add('estilo-p')
+        let liDescricaoEmpresa = document.createElement('li')
+        liDescricaoEmpresa.classList.add('dados-usuario')
+        liDescricaoEmpresa.innerHTML = empresa.descricao
+
+        let br = document.createElement("br")
+
+        ul.appendChild(pNomeEmpresa)
+        ul.appendChild(liNomeEmpresa)
+        ul.appendChild(pEmailEmpresa)
+        ul.appendChild(liEmailEmpresa)
+        ul.appendChild(pCnpjEmpresa)
+        ul.appendChild(liCnpjEmpresa)
+        ul.appendChild(pCepEmpresa)
+        ul.appendChild(liCepEmpresa)
+        ul.appendChild(pEstadoEmpresa)
+        ul.appendChild(liEstadoEmpresa)
+        ul.appendChild(pPaisEmpresa)
+        ul.appendChild(liPaisEmpresa)
+        ul.appendChild(pDescricaoEmpresa)
+        ul.appendChild(liDescricaoEmpresa)
+        div.appendChild(ul)
+        div.appendChild(br)
+
+        return div
+    }
+
+    mostrarListaEmpresas()
+
+    /* GRÁFICO */
+
+    let listaCandidatosLocalStorage = localStorage.getItem('candidatos') as string
+    let git: number = 0
+    let metodologiasAgeis: number = 0
+    let testes: number = 0
+    let java: number = 0
+    let groovy: number = 0
+    let javascript: number = 0
+    let typescript: number = 0
+    if(listaCandidatosLocalStorage) {
+        const listaCandidatosJson = JSON.parse(listaCandidatosLocalStorage)
+        for(let competencias = 0; competencias < listaCandidatosJson.length; competencias++) {
+            let listaCompetencias = listaCandidatosJson[competencias].competencias.split(',')
+            for(let capturaCompetencia = 0; capturaCompetencia < listaCompetencias.length; capturaCompetencia++) {
+                if(listaCompetencias[capturaCompetencia] == "Git") {
+                    git += 1
+                }
+                if(listaCompetencias[capturaCompetencia] == "Metodologias Ageis") {
+                    metodologiasAgeis += 1
+                }
+                if(listaCompetencias[capturaCompetencia] == "Testes") {
+                    testes += 1
+                }
+                if(listaCompetencias[capturaCompetencia] == "Java") {
+                    java += 1
+                }
+                if(listaCompetencias[capturaCompetencia] == "Groovy") {
+                    groovy += 1
+                }
+                if(listaCompetencias[capturaCompetencia] == "JavaScript") {
+                    javascript += 1
+                }
+                if(listaCompetencias[capturaCompetencia] == "TypeScript") {
+                    typescript += 1
+                }
+
+            }
+        }
+    }
+
+    const labels = [
+        'Git',
+        'Metodologias Ágeis',
+        'Testes',
+        'Java',
+        'Groovy',
+        'JavaScript',
+        'TypeScript'
+    ]
+
+    const data = {
+        labels,
+        datasets: [{
+            data: [git, metodologiasAgeis, testes, java, groovy, javascript, typescript],
+            label: "Gráfico Competências dos Candidatos"
+        }]
+    }
+
+    const config = {
+        type: 'doughnut',
+        data: data,
+        options: {
+            responsive: true,
+            soucemap: false
+        }
+    }
+
+    const myChart = new Chart(canvas, config)
 
 })
