60f2f78f7b94e8d971471f972ff69ac3f0357b20
Higor Cirne <cirnne1@outlook.com>
2023-03-13 20:45:49 -0300

    feat: Banco de Dados(Adcionado os comandos de inserção de matchs no arquivo comandos_sql_linketinder.sql)

--- banco_dados/comandos_sql_linketinder.sql
+++ banco_dados/comandos_sql_linketinder.sql
@@ -1,39 +1,39 @@
 -- Pais:
 -- CREATE TABLE "pais" (
 --  "id" SERIAL PRIMARY KEY,
---  "nome" VARCHAR(15) NOT NULL
+--  "nome" varchar(15) NOT NULL
 -- );
 
 -- Candidatos:
 -- CREATE TABLE "candidatos" (
 --  "id" SERIAL PRIMARY KEY,
---  "nome" VARCHAR(100) NOT NULL,
+--  "nome" varchar(100) NOT NULL,
 --  "data_nascimento" DATE NOT NULL,
---  "email" VARCHAR(100) NOT NULL,
---  "cpf" BIGINT NOT NULL,
+--  "email" varchar(100) NOT NULL,
+--  "cpf" bigint NOT NULL,
 --  "id_pais" INT REFERENCES pais(id)NOT NULL,
---  "cep" INT NOT NULL,
---  "descricao_pessoal" VARCHAR(250) NOT NULL,
---  "senha" VARCHAR(50) NOT NULL
+--  "cep" int NOT NULL,
+--  "descricao_pessoal" varchar(250) NOT NULL,
+--  "senha" varchar(50) NOT NULL
 -- );
 
 -- Empresas:
 --CREATE TABLE "empresas" (
 --  "id" SERIAL PRIMARY KEY,
---  "nome" VARCHAR(100) NOT NULL,
---  "cnpj" BIGINT NOT NULL,
---  "email" VARCHAR(100) NOT NULL,
---  "descricao_empresa" VARCHAR(250) NOT NULL,
+--  "nome" varchar(100) NOT NULL,
+--  "cnpj" bigint NOT NULL,
+--  "email" varchar(100) NOT NULL,
+--  "descricao_empresa" varchar(250) NOT NULL,
 --  "id_pais" INT REFERENCES pais(id) NOT NULL,
---  "cep" INT NOT NULL,
---  "senha" VARCHAR(50) NOT NULL
+--  "cep" int NOT NULL,
+--  "senha" varchar(50) NOT NULL
 -- );
 
 -- Vagas:
 -- CREATE TABLE "vagas" (
 --  "id" SERIAL PRIMARY KEY,
---  "nome" VARCHAR(50) NOT NULL,
---  "descricao_vaga" VARCHAR(250) NOT NULL,
+--  "nome" varchar(50) NOT NULL,
+--  "descricao_vaga" varchar(250) NOT NULL,
 --  "id_empresa" INT REFERENCES empresas(id) NOT NULL,
 --  "id_pais" INT REFERENCES pais(id) NOT NULL
 -- );
@@ -41,7 +41,7 @@
 -- Competências:
 -- CREATE TABLE "competencias" (
 --  "id" SERIAL PRIMARY KEY,
---  "nome" VARCHAR(100) NOT NULL,
+--  "nome" varchar(100) NOT NULL,
 --  "id_candidato" INT REFERENCES candidatos(id) NOT NULL,
 --  "id_vagas" INT REFERENCES vagas(id) NOT NULL
 -- );
@@ -49,7 +49,7 @@
 -- Modelo de Trabalho:
 -- CREATE TABLE "modelo_trabalho" (
 --  "id" SERIAL PRIMARY KEY,
---  "nome" VARCHAR(10) NOT NULL,
+--  "nome" varchar(10) NOT NULL,
 --  "id_vaga" INT REFERENCES vagas(id) NOT NULL
 -- );
 
@@ -94,3 +94,27 @@
 -- INSERT INTO empresas(nome, cnpj, email, descricao_empresa, id_pais, cep, senha) VALUES('Empresa Três', 33333333333333, 'tresdeempresa@acelerazg.com.br', 'descrição empresa 3', 1, 15935782, '3senha!empresa');
 -- INSERT INTO empresas(nome, cnpj, email, descricao_empresa, id_pais, cep, senha) VALUES('Empresa Quarto', 44444444444444, 'Aempresa4@acelerazg.com.br', 'descrição empresa 4', 1, 95175328, 'four@empresa4');
 -- INSERT INTO empresas(nome, cnpj, email, descricao_empresa, id_pais, cep, senha) VALUES('Empresa Cinco', 55555555555555, 'EmpresaCINCO@acelerazg.com.br', 'descrição empresa 5', 1, 31649746, 'empresa5@Cinco');
+
+-- Inserindo Curtida do Candidato:
+-- INSERT INTO curtida_candidato(id_candidato, id_empresa) VALUES(1, 2);
+-- INSERT INTO curtida_candidato(id_candidato, id_empresa) VALUES(4, 1);
+
+-- SELECT c.nome AS "Nome Candidato", e.nome AS "Nome Empresa"
+--	FROM candidatos AS c, empresas AS e, curtida_candidato AS ca
+--		WHERE ca.id_candidato = c.id  AND ca.id_empresa = e.id;
+
+-- Inserindo Curtida da Empresa:
+-- INSERT INTO curtida_empresa(id_empresa, id_candidato) VALUES(2, 1);
+-- INSERT INTO curtida_empresa(id_empresa, id_candidato) VALUES(1, 4);
+
+--SELECT e.nome AS "Nome Empresa", c.nome AS "Nome Candidato"
+--	FROM curtida_empresa AS ce, empresas AS e, candidatos AS c
+--		WHERE ce.id_empresa = e.id AND ce.id_candidato = c.id;
+
+-- Inserindo Match:
+-- INSERT INTO match(id_candidato, id_empresa) VALUES(1, 2);
+-- INSERT INTO match(id_candidato, id_empresa) VALUES(4, 1);
+
+-- SELECT c.nome AS "Nome Candidato", e.nome AS "Nome Empresa"
+-- FROM match AS m, candidatos AS c, empresas AS e
+-- WHERE m.id_candidato = c.id AND m.id_empresa = e.id;
